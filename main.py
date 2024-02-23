10d2f0c2734df2c3ab5cf4bd4759eb91383bda0d
msquaredds <alex.melesko@msquaredds.com>
2024-02-23 12:26:04 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+def main():
+    table_display_cols = st.columns([1, 3, 1])
+    with table_display_cols[1]:
+        with st.container(border=True):
+            row_cols = st.columns(3)
+            with row_cols[0]:
+                # set a key so the text_inputs don't clash and
+                # we can access them later if necessary
+                key = "main_input_key"
+                user_number = st.number_input(
+                    '', value=100.0, step=0.1,
+                    format="%0.1f", key=key,
+                    label_visibility='collapsed')
+
+
+if __name__ == '__main__':
+    main()
