63dd1362b4d9ebad18e7917166c6cde8c36abf59
msquaredds <alex.melesko@msquaredds.com>
2024-02-23 12:33:47 -0500

    Removing key and the TestClass file

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ def main():
         with st.container(border=True):
             row_cols = st.columns(3)
             with row_cols[0]:
-                user_number = st.number_input(
-                    '', value=100.0)
+                user_number = st.number_input('This is a number input')
 
 
 if __name__ == '__main__':
