85c3af85b48fba51cbb6e2d8527eba6cd4eaba64
msquaredds <alex.melesko@msquaredds.com>
2024-02-23 12:33:03 -0500

    Removing key and the TestClass file

--- main.py
+++ main.py
@@ -7,11 +7,8 @@ def main():
         with st.container(border=True):
             row_cols = st.columns(3)
             with row_cols[0]:
-                # set a key so the text_inputs don't clash and
-                # we can access them later if necessary
                 user_number = st.number_input(
-                    '', value=100.0, step=0.1,
-                    label_visibility='collapsed')
+                    '', value=100.0, step=0.1)
 
 
 if __name__ == '__main__':
